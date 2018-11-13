eecf8b8ecd48d22058bbd95a29a36d562aca412c
Yuta Yamaguchi <yuta.yamaguchi@gmail.com>
2018-11-13 14:44:04 -0600

    update

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoieXV0YXkiLCJhIjoiY2puejJncG1nMWJ2MDNwa2pzNTBuNjh2ZyJ9.pRnUnLn68JxmNSLbICTrLw";
---
+++ static/js/logic.js
@@ -0,0 +1,11 @@
+var myMap = L.map("mapbox", {
+    center: [30.26, -97.74],
+    zoom: 13
+  });
+
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
