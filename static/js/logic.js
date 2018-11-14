ac4645d6f579e9c51b8a250280f6bb683acb3984
Yuta Yamaguchi <yuta.yamaguchi@gmail.com>
2018-11-13 20:02:02 -0600

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-var myMap = L.map("mapbox", {
+var myMap = L.map("map", {
     center: [30.26, -97.74],
     zoom: 13
   });
@@ -6,6 +6,6 @@ var myMap = L.map("mapbox", {
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.streets",
+  id: "mapbox.light",
   accessToken: API_KEY
 }).addTo(myMap);
