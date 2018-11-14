cea690c8b437d9145c4bf3f82341552187ba4560
Yuta Yamaguchi <yuta.yamaguchi@gmail.com>
2018-11-13 21:37:49 -0600

    update

---
+++ static/js/temp.js
@@ -0,0 +1,21 @@
+document.getElementById("Save").addEventListener('click',function ()
+    {
+     alert("hello");
+     //validation code to see State field is mandatory.  
+    }  ); 
+
+<button onclick='test()' >click</button>
+
+//
+<button onclick="myFunction()">Click me</button>
+
+<p id="demo"></p>
+
+<p>A function is triggered when the button is clicked. The function outputs some text in a p element with id="demo".</p>
+
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+</script>
+//
